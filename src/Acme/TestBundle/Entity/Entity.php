dd777f798585558b1ae2c8cc875f6f764d6208a0
marcosQuesada <marcos.quesadas@gmail.com>
2012-09-26 01:21:45 +0200

    add getset magic function skeleton, last commit

--- src/Acme/TestBundle/Entity/Entity.php
+++ src/Acme/TestBundle/Entity/Entity.php
@@ -4,15 +4,24 @@ namespace Acme\TestBundle\Entity;
  
 class Entity
 {
+    public function add($key, $value, $type)
+    {
+        if ($type == 'integer')
+            $this->$key = (int)$value;
+        if ($type == 'string')
+            $this->$key = (string)$value;       
+        if ($type == 'boolean')
+            $this->$key = (bool)$value; 
+        return $this;
+    }
 
-	public function add($key, $value, $type)
-	{
-		if ($type == 'integer')
-			$this->$key = (int)$value;
-		if ($type == 'string')
-			$this->$key = (string)$value;		
-		if ($type == 'boolean')
-			$this->$key = (bool)$value;	
-		return $this;
-	}
+    public function __call($arg1, $arg2)
+    {
+
+    }
+
+    public function __set($arg1, $arg2)
+    {
+
+    }
 }
