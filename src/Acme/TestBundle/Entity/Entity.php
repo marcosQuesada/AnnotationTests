c65222a9780c3fe734f1c6daf56b78ec9750c05b
Marcos Quesada <marcos.quesadas@gmail.com>
2012-09-26 11:35:00 +0300

    Update src/Acme/TestBundle/Entity/Entity.php
    Removed chainned conditional, replaced with settype.

--- src/Acme/TestBundle/Entity/Entity.php
+++ src/Acme/TestBundle/Entity/Entity.php
@@ -6,12 +6,7 @@ class Entity
 {
     public function add($key, $value, $type)
     {
-        if ($type == 'integer')
-            $this->$key = (int)$value;
-        if ($type == 'string')
-            $this->$key = (string)$value;       
-        if ($type == 'boolean')
-            $this->$key = (bool)$value; 
+        settype($value, $type);
         return $this;
     }
 
